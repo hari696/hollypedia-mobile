1c32b8730d9ebbc8a58ee393b3c1fc8b1d2d0cbd
Napster <harinapster007@gmail.com>
2017-10-20 18:46:44 +0530

    Home Component

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import Home from './components/home';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to HollyPedia!
-        </Text>
-        <Text style={styles.instructions}>
-          Enjoy and Grow your HollyWood knowledge
-        </Text>
-        <Text style={styles.instructions}>
-          All Popular and Other List of movies are avaliable on Door Step.
-        </Text>
-      </View>
+      <Home />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
